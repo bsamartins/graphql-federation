dd00949eba05884952c754c4cd8f78400b273db4
Bernardo Martins <bsamartins@gmail.com>
2022-04-18 19:23:47 +0100

    Rename method

--- graphql-quarkus/federation/deployment/src/main/java/io/bsamartins/graphql/quarkus/federation/deployment/apollo/ApolloFederation.kt
+++ graphql-quarkus/federation/deployment/src/main/java/io/bsamartins/graphql/quarkus/federation/deployment/apollo/ApolloFederation.kt
@@ -4,18 +4,21 @@ import com.apollographql.federation.graphqljava.Federation
 import com.apollographql.federation.graphqljava.FederationDirectives
 import graphql.schema.GraphQLSchema
 import graphql.schema.GraphQLSchema.Builder
+import io.bsamartins.graphql.quarkus.federation.deployment.runtime.apollo.Key
+import org.jboss.jandex.DotName
 import javax.enterprise.context.ApplicationScoped
 import javax.enterprise.event.Observes
 
 @ApplicationScoped
 class ApolloFederation {
 
-    fun observe(@Observes builder: Builder): Builder {
+    private val KEY = DotName.createSimple(Key::class.java.name)
+
+    fun apply(@Observes builder: Builder): Builder {
         FederationDirectives.allDirectives.forEach {
             builder.additionalDirective(it)
         }
         val schema = builder.build()
-
         return Federation.transform(schema)
             .build()
             .let { GraphQLSchema.newSchema(it) }
---
+++ graphql-quarkus/federation/runtime/src/main/kotlin/io/bsamartins/graphql/quarkus/federation/deployment/runtime/apollo/Key.kt
@@ -0,0 +1,3 @@
+package io.bsamartins.graphql.quarkus.federation.deployment.runtime.apollo
+
+annotation class Key()
